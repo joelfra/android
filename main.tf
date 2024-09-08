62b97214a802c82e8352cbc2ff54d73dd1d54f9f
joelfra <61116394+joelfra@users.noreply.github.com>
2024-09-08 10:11:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
   name                     = "tt-githubdemo-bucket-001"
-  project                  = "tt-dev-001"
+  project                  = "producteur"
   location                 = "europe-west9"
   force_destroy            = true
   public_access_prevention = "enforced"
